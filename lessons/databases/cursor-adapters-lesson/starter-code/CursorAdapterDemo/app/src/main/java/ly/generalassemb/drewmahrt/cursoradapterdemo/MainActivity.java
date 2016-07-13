f9ccada9c42ff816cadffc45eee72f2d7cd2d528
Joey <joeymejias@gmail.com>
2016-07-13 11:03:44 -0400

    working hard for that money

--- lessons/databases/cursor-adapters-lesson/starter-code/CursorAdapterDemo/app/src/main/java/ly/generalassemb/drewmahrt/cursoradapterdemo/MainActivity.java
+++ lessons/databases/cursor-adapters-lesson/starter-code/CursorAdapterDemo/app/src/main/java/ly/generalassemb/drewmahrt/cursoradapterdemo/MainActivity.java
@@ -1,20 +1,111 @@
 package ly.generalassemb.drewmahrt.cursoradapterdemo;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import ly.generalassemb.drewmahrt.cursoradapterdemo.setup.DBAssetHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mListView = (ListView) findViewById(R.id.example_list_view);
+
         //Ignore the two lines below, they are for setup
         DBAssetHelper dbSetup = new DBAssetHelper(MainActivity.this);
         dbSetup.getReadableDatabase();
+
+        Cursor cursor = ExampleSQLiteOpenHelper
+                .getInstance(MainActivity.this)
+                .getExampleList();
+
+        CursorAdapter adapter = new CursorAdapter(MainActivity.this, cursor,
+                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER) {
+            @Override
+            public View newView(Context context, Cursor cursor, ViewGroup parent) {
+                return LayoutInflater.from(context)
+                        .inflate(android.R.layout.simple_expandable_list_item_1, parent, false);
+            }
+
+            @Override
+            public void bindView(View view, Context context, Cursor cursor) {
+                TextView textView = (TextView) view.findViewById(android.R.id.text1);
+
+                int index = cursor.getColumnIndex(ExampleSQLiteOpenHelper.COL_ITEM_NAME);
+
+                textView.setText(cursor.getString(index));
+            }
+        };
+
+        CursorAdapter customAdapter = new CursorAdapter(MainActivity.this, cursor,
+                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER) {
+            @Override
+            public View newView(Context context, Cursor cursor, ViewGroup parent) {
+                return LayoutInflater.from(context)
+                        .inflate(R.layout.list_item_layout, parent, false);
+            }
+
+            @Override
+            public void bindView(View view, Context context, Cursor cursor) {
+                TextView nameTextView = (TextView)
+                        view.findViewById(R.id.textview_name);
+                TextView descriptionTextView = (TextView)
+                        view.findViewById(R.id.textview_description);
+
+                int nameIndex = cursor.getColumnIndex(ExampleSQLiteOpenHelper.COL_ITEM_NAME);
+                int descriptionIndex = cursor.getColumnIndex(ExampleSQLiteOpenHelper.COL_ITEM_DESCRIPTION);
+
+                nameTextView.setText(cursor.getString(nameIndex));
+                descriptionTextView.setText(cursor.getString(descriptionIndex));
+
+            }
+        };
+
+        String[] columnNames = new String[]{ExampleSQLiteOpenHelper.COL_ITEM_NAME};
+        int[] viewNames = new int[]{android.R.id.text1};
+
+        SimpleCursorAdapter simpleCursorAdapter =
+                new SimpleCursorAdapter(MainActivity.this,
+                        android.R.layout.simple_list_item_1,
+                        cursor,
+                        columnNames,
+                        viewNames,
+                        CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+
+        String[] customColumnNames = new String[]{
+                ExampleSQLiteOpenHelper.COL_ITEM_NAME,
+                ExampleSQLiteOpenHelper.COL_ITEM_DESCRIPTION
+        };
+
+        int[] customViewNames = new int[]{R.id.textview_name, R.id.textview_description};
+
+        SimpleCursorAdapter customSimpleCursorAdapter =
+                new SimpleCursorAdapter(MainActivity.this,
+                        R.layout.list_item_layout,
+                        cursor,
+                        customColumnNames,
+                        customViewNames,
+                        CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+
+//      mListView.setAdapter(adapter); //First Adapter
+//      mListView.setAdapter(customAdapter); //Second Adapter
+        mListView.setAdapter(customSimpleCursorAdapter);
 
     }
 }
--- lessons/databases/cursor-adapters-lesson/starter-code/IconList/app/src/main/java/ly/generalassemb/drewmahrt/iconlist/MainActivity.java
+++ lessons/databases/cursor-adapters-lesson/starter-code/IconList/app/src/main/java/ly/generalassemb/drewmahrt/iconlist/MainActivity.java
@@ -1,21 +1,62 @@
 package ly.generalassemb.drewmahrt.iconlist;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.media.Image;
 import android.os.Bundle;
+import android.support.v4.widget.CursorAdapter;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import ly.generalassemb.drewmahrt.iconlist.setup.DBAssetHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mListView = (ListView) findViewById(R.id.listview_icons);
+
         //Ignore the two lines below
         DBAssetHelper dbAssetHelper = new DBAssetHelper(MainActivity.this);
         dbAssetHelper.getReadableDatabase();
 
+        Cursor cursor = IconSQLiteOpenHelper
+                .getInstance(this).getIconList();
+
+        CursorAdapter adapter = new CursorAdapter(MainActivity.this, cursor, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER) {
+            @Override
+            public View newView(Context context, Cursor cursor, ViewGroup parent) {
+                return LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.icon_list_item, parent, false);
+            }
+
+            @Override
+            public void bindView(View view, Context context, Cursor cursor) {
+                ImageView icon = (ImageView) view.findViewById(R.id.imageview_icon);
+                TextView iconName = (TextView) view.findViewById(R.id.textview_icon_name);
+
+                int colIndex = cursor.getColumnIndex(IconSQLiteOpenHelper.COL_ICON_NAME);
+                String name = cursor.getString(colIndex);
+
+                iconName.setText(name);
+
+                int drawableValue = getDrawableValue(name);
+                icon.setImageResource(drawableValue);
+            }
+        };
+
+        mListView.setAdapter(adapter);
+        cursor.close();
     }
 
     private int getDrawableValue(String icon){
