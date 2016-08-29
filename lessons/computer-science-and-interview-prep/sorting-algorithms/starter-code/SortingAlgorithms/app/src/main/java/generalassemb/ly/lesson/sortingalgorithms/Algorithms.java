744adb65dd16beedc77572873277221451d76ce2
Joey <joeymejias@gmail.com>
2016-08-29 10:17:35 -0400

    sorting

--- lessons/computer-science-and-interview-prep/sorting-algorithms/starter-code/SortingAlgorithms/app/src/main/java/generalassemb/ly/lesson/sortingalgorithms/Algorithms.java
+++ lessons/computer-science-and-interview-prep/sorting-algorithms/starter-code/SortingAlgorithms/app/src/main/java/generalassemb/ly/lesson/sortingalgorithms/Algorithms.java
@@ -10,7 +10,25 @@ public class Algorithms {
             Code in your bubble sort algorithm
             It should perform the algorithm "in-place".
             Meaning, DO NOT MAKE A COPY OF THE ARGUMENT
+
+            for i from 1 to N
+                for j from 0 to N - 1
+                    if a[j] > a[j + 1]
+                        swap a[j] and a[j + 1]
+            return N
          */
+
+        for (int i = 1; i < array.length; i++) {
+            for (int j = 0; j < array.length-1; j++) {
+                if (array[j] > array[j + 1]) { // change to array[j] < array[j + 1] for descending order
+                    //swap logic
+                    int tempNum = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = tempNum;
+                }
+            }
+        }
+
         return array;
     }
 
@@ -20,6 +38,16 @@ public class Algorithms {
             It should perform the algorithm "in-place"
             Meaning, DO NOT MAKE A COPY OF THE ARGUMENT
          */
+
+        for (int i = 1; i < array.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (array[j] < array[j-1]) {
+                    int tempNum = array[j];
+                    array[j] = array[j-1];
+                    array[j-1] = tempNum;
+                }
+            }
+        }
         return array;
     }
 
